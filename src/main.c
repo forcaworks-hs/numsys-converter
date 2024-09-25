013be3bbf52993c1e372f1c3b96b460c4e8cb3ad
F0RCAD31T0R <forcadeitor1@gmail.com>
2024-09-25 18:02:44 +0200

    build: added usage instructions

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    
+int main(int argc, char **argv) {
+    if (argc !=2) {
+        printf("Usage %s <number> <option>\n", argv[0]);
+        return 1;
+    }
     
     return 0;
 }
+
+
